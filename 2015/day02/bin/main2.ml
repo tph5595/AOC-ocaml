1a064a399098b3dd3bf106d0b9eafef30ded5167
Taylor Henderson <taylor.henderson@jhuapl.edu>
2024-08-28 11:35:43 -0400

    fold em

---
+++ 2015/day02/bin/main2.ml
@@ -0,0 +1,67 @@
+open Core;;
+
+type box = {
+    l: int;
+    w: int;
+    h: int;
+};;
+
+let make_box dims =
+    (* List.iter (Printf.printf "%d ") dims; *)
+    let lis = List.map ~f:Int.of_string dims in 
+    match lis with
+    | [l; w; h;] -> {l; w; h;}
+    | _ -> {l=0; w=0; h=0;}
+;;
+
+let sum_box { l; w; h } = 
+    let sides = [ l*w; w*h; h*l ] in 
+    let sum = List.fold_left ~f:(+) ~init:0 sides in 
+    let extra = BatList.min sides in 
+    2*sum + extra
+;;
+
+let sum_ribbon { l; w; h } = 
+    let sides = BatList.min [ l+w; w+h; h+l ] in 
+    let bow = l*w*h in 
+    2*sides + bow
+;;
+
+let get_dims str = 
+    str
+    |> String.split_on_chars ~on:[ 'x' ]
+    |> List.filter ~f:(fun s -> not (String.equal s ""))
+;;
+
+let split_boxes str = 
+    str
+    |> String.split_on_chars ~on:[ '\n' ; '\r' ]
+    |> List.filter ~f:(fun s -> not (String.equal s "")) 
+;;
+
+let parse_box file_contents = 
+    let lines = split_boxes file_contents in 
+    let dims = List.map ~f:get_dims lines in 
+    let boxes = List.map ~f:(fun dim -> make_box dim ) dims in 
+    boxes
+;;
+
+
+let () = 
+    let data = "input.txt"
+    |> In_channel.read_all 
+    |> parse_box 
+    in 
+
+    let result = data
+    |> List.map ~f:sum_box
+    |> List.fold_left ~f:(+) ~init:0 
+    in 
+    Printf.printf "%d\n" result;
+
+    let result = data
+    |> List.map ~f:sum_ribbon
+    |> List.fold_left ~f:(+) ~init:0 
+    in 
+    Printf.printf "%d\n" result
+;;
