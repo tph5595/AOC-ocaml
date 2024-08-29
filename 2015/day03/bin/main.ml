333a264c4931bdfad9b201e94d183e9d31ae1092
Taylor Henderson <tph5595@verizon.net>
2024-08-29 13:21:53 -0400

    part 1

--- 2015/day03/bin/main.ml
+++ 2015/day03/bin/main.ml
@@ -3,23 +3,35 @@ open Core;;
 module Point = struct
     module T = struct
         type t = { x: int; y: int }
-
-        let compare t1 t2 = 
-            let xs = Int.compare t1.x t2.x in
-            if xs <> 0 then xs
-            else Int.compare t1.y t2.y
-        
-        let sexp_of_t t: Sexp.t = 
-            List [Atom (string_of_int t.x); Atom (string_of_int t.y)]
+        [@@deriving compare, sexp_of]
     end
     include T 
     include Comparator.Make(T)
 end;;
 
-let s = Set.of_list (module Point) [{x = 0; y = 0}; {x = 1; y = 0}];;
+let solve_part_1 input = 
+    let open Point.T in 
+    let rec aux input cur  hash_set = 
+        match input with 
+        | c :: tail ->
+            let n = match c with 
+                | '>' -> {cur with x = cur.x + 1}
+                | '<' -> {cur with x = cur.x - 1}
+                | '^' -> {cur with y = cur.y + 1}
+                | 'v' -> {cur with y = cur.y - 1}
+                | _ -> cur
+            in 
+            aux tail n (Set.add hash_set n)
+        | _ -> hash_set
+    in 
+    let pos = {x = 0; y = 0} in 
+    let set = aux (String.to_list input) pos (Set.of_list (module Point) [ pos ]) in
+    Set.length set
+;;
 
 let () = 
     let data = "input.txt"
     |> In_channel.read_all
     in 
-    print_endline data
+    let part1 = solve_part_1 data in 
+    Printf.printf "%d\n" part1
