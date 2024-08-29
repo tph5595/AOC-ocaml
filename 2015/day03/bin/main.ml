8869bef5974f7cec0a9224752fa3794f6911d361
Taylor Henderson <tph5595@verizon.net>
2024-08-29 13:26:32 -0400

    part 2. ez

--- 2015/day03/bin/main.ml
+++ 2015/day03/bin/main.ml
@@ -11,7 +11,7 @@ end;;
 
 let solve_part_1 input = 
     let open Point.T in 
-    let rec aux input cur  hash_set = 
+    let rec aux input cur hash_set = 
         match input with 
         | c :: tail ->
             let n = match c with 
@@ -29,9 +29,31 @@ let solve_part_1 input =
     Set.length set
 ;;
 
+let solve_part_2 input = 
+    let open Point.T in 
+    let rec aux input cur1 cur2 hash_set = 
+        match input with 
+        | c :: tail ->
+            let n = match c with 
+                | '>' -> {cur1 with x = cur1.x + 1}
+                | '<' -> {cur1 with x = cur1.x - 1}
+                | '^' -> {cur1 with y = cur1.y + 1}
+                | 'v' -> {cur1 with y = cur1.y - 1}
+                | _ -> cur1
+            in 
+            aux tail cur2 n (Set.add hash_set n)
+        | _ -> hash_set
+    in 
+    let pos1 = {x = 0; y = 0} in 
+    let pos2 = {x = 0; y = 0} in 
+    let set = aux (String.to_list input) pos1 pos2 (Set.of_list (module Point) [ pos1 ]) in
+    Set.length set
+;;
+
 let () = 
     let data = "input.txt"
     |> In_channel.read_all
     in 
     let part1 = solve_part_1 data in 
-    Printf.printf "%d\n" part1
+    let part2 = solve_part_2 data in 
+    Printf.printf "%d\n%d\n" part1 part2
